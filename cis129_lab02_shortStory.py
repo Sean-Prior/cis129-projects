ed507c1c60551551eb7a933561e3496ba3645fcf
Sean-Prior <sprior1@mail.pima.edu>
2024-09-15 19:50:32 -0700

    Update cis129_lab02_shortStory.py
    Removes Hello world and makes it into a short story with comments on top.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print('Hello, World!')
+# Author Sean Prior
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('This is a short story about a print statement that used to say "hello world" but now instead prints a short story about a print statement instead of "hello world". Normally, a short story is ment to be interesting or provide something to think about. However, this short story just tells you what this print statement once was printing, pretty cool huh?')
