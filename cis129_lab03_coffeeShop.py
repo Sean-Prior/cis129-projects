05ebfe649d2229aa531ae5748ba5591b8424532a
Sean-Prior <sprior1@mail.pima.edu>
2024-09-22 22:41:56 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,12 +1,27 @@
-#This is the coffee shop
+# Sean Prior
+# This is the coffee shop lab
+# Asks the user how many muffins and coffees they want, then gives them the total.
+print("***************************************")
 print("My Coffee and Muffin Shop")
+# This creates the integers and required inputs from the user for the coffee shop
 coffeeQuestion = int(input("Number of coffees bought?\n"))
 muffinQuestion = int(input("Number of muffins bought?\n"))
+teaQuestion = int(input("Number of teas bought?\n"))
+bagelQuestion = int(input("Number of bagels bought?\n"))
+print("***************************************\n")
+# This does the required math to get the correct value for the coffee shop
 muffinCost = muffinQuestion * 4
 coffeeCost = coffeeQuestion * 5
-tax = (muffinCost + coffeeCost)*.06
-
-print(coffeeQuestion, "Coffee at $5 each: ", coffeeCost)
-print(muffinQuestion, "muffins at 4$ each: ", muffinCost)
+teaCost = teaQuestion * 3
+bagelCost = bagelQuestion * 5
+tax = (muffinCost + coffeeCost + teaCost + bagelCost)*.06
+# This prints the totals like a receipt at an actual coffee shop
+print("***************************************")
+print(coffeeQuestion, "Coffee at $5 each: $", coffeeCost)
+print(muffinQuestion, "muffins at 4$ each: $", muffinCost)
+print(teaQuestion, "tea at $3 each: $", teaCost)
+print(bagelQuestion, "bagel at $5 each: $", bagelCost)
 print("6% tax: ", tax)
-print("Total: $" , muffinCost + coffeeCost + tax)
+print("---------")
+print("Total: $" , muffinCost + coffeeCost + teaCost + bagelCost + tax)
+print("***************************************")
